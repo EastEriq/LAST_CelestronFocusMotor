739893616def42e440f749435a4576b77bc96e47
Enrico Segre <enrico.segre@weizmann.ac.il>
2020-08-26 08:32:46 +0000

    check availability of serial resource before every query

---
+++ +inst/@CelestronFocuser/private/isPortAvailable.m
@@ -0,0 +1,18 @@
+function avail=isPortAvailable(F)
+% Check if the serial resource assigned to the focuser object is
+%  still known to the system, and delete if it disappeared.
+% This is often necessary as virtual serial ports, assigned to
+%  serial-USB devices frequently disconnect and reconnect under
+%  another name, due to EMI, poor cables and whatnot.
+   
+    portlist=serialportlist; % use seriallist in rev<2019 instead
+    
+    avail=any(contains(portlist,F.Port));
+    if ~avail
+        F.report("Serial "+F.Port+' disappeared from system, closing it\n')
+        try
+            delete(instrfind('Port',F.Port))
+        catch
+            F.lastError=['cannot delete Port object ' F.Port ' -maybe OS disconnected it?'];
+        end
+    end
--- +inst/@CelestronFocuser/private/query.m
+++ +inst/@CelestronFocuser/private/query.m
@@ -1,19 +1,28 @@
         function resp=query(N,dest,cmd,data)
             % Send a command and sit waiting for its reply
-            % Dispose of previous traffic potentially having
-            % filled the inbuffer, for an immediate response
-            flushinput(N.serial_resource)
-            % just reading all bytes sometimes is not enough
-            %  when the buffer is full since a long time, the expected 
-            %  reply gets lost anyway (don't know why)
-%             if N.Port.BytesAvailable>0
-%                 %disp(['purging buffer with ' num2str(N.Port.BytesAvailable) ' bytes'])
-%                 fread(N.Port,N.Port.BytesAvailable);
-%             end
-            if exist('data','var')
-                N.send(dest,cmd,data);
+            
+            % check if the serial port is still reachable first.
+            % Disconnections are so frequent
+            if ~isPortAvailable(N)
+                % ask David which kind of error reporting we expect at
+                %  abstraction level: error, F.lastError, stdout, what?
+                error(['Port ' N.Port 'disappeared'])
             else
-                N.send(dest,cmd);
+                % Dispose of previous traffic potentially having
+                % filled the inbuffer, for an immediate response
+                flushinput(N.serial_resource)
+                % just reading all bytes sometimes is not enough
+                %  when the buffer is full since a long time, the expected
+                %  reply gets lost anyway (don't know why)
+                %             if N.Port.BytesAvailable>0
+                %                 %disp(['purging buffer with ' num2str(N.Port.BytesAvailable) ' bytes'])
+                %                 fread(N.Port,N.Port.BytesAvailable);
+                %             end
+                if exist('data','var')
+                    N.send(dest,cmd,data);
+                else
+                    N.send(dest,cmd);
+                end
+                resp=N.waitResponse(dest,cmd);
             end
-            resp=N.waitResponse(dest,cmd);
         end
