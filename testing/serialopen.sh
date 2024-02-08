7f219889300c9ff4955b3f0c8fa2e648605c7ff1
The LAST Ocs <>
2024-02-08 17:43:54 +0000

    also stress test Unit + connect mount

--- testing/serialopen.sh
+++ testing/serialopen.sh
@@ -5,8 +5,9 @@ echo > testing.log
 i=0
 while [ $i -le 1000 ]
 do
-   echo "----------------------" $i | tee -a testing.log
-   time matlab -nosplash -r "datetime, F=inst.CelestronFocuser('11_1_1'); F.connect; F.Status, exit" | tee -a testing.log
+   echo `date` "----------------------" $i | tee -a testing.log
+#   time matlab -nosplash -r "datetime, F=inst.CelestronFocuser('11_1_1'); F.connect; F.Status, exit" | tee -a testing.log
+   time matlab -nosplash -r "Unit=obs.unitCS('00'); Unit.connect; exit" | tee -a testing.log
    echo $i "exit status: " $? | tee -a testing.log
    ((i++))
 done
