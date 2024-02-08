5715d8112a80e1e580d1c65d3ff1ead53ee3fd3e
The LAST Ocs <>
2024-02-08 09:52:40 +0000

    improved stress testing bash script

--- testing/serialopen.sh
+++ testing/serialopen.sh
@@ -3,9 +3,10 @@
 echo > testing.log
 
 i=0
-while [ $i -le 100 ]
+while [ $i -le 1000 ]
 do
-   echo "----------------------" $i >> testing.log
-   time matlab -batch "F=inst.CelestronFocuser('11_1_1'); F.connect" >> testing.log
+   echo "----------------------" $i | tee -a testing.log
+   time matlab -nosplash -r "datetime, F=inst.CelestronFocuser('11_1_1'); F.connect; F.Status, exit" | tee -a testing.log
+   echo $i "exit status: " $? | tee -a testing.log
    ((i++))
 done
