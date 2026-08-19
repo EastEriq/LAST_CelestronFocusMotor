fe565544d62fd8b920897b57ecd2b314adaee1a3
Enrico Segre <enrico.segre@weizmann.ac.il>
2026-08-19 17:23:57 +0300

    remove useless `.report`s in calibrate (there are `.reportError`s)

--- +inst/@CelestronFocuser/calibrate.m
+++ +inst/@CelestronFocuser/calibrate.m
@@ -28,7 +28,6 @@ function calibrate(F)
             lastpos(end)=F.Pos;
             F.report('... t=%.1f, calibration stage %d, f=%d\n',t,stage,lastpos(end))
             if all(lastpos==lastpos(end))
-                F.report('Focuser stuck!')
                 F.reportError('Focuser stuck during calibration!');
                 break
             end
@@ -38,7 +37,6 @@ function calibrate(F)
             F.LastError='';
             F.TargetPos=NaN;
         else
-            F.report('Calibration timed out!\n')
             F.reportError('Calibration timed out!');
         end
     catch
